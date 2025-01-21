0a34904420f354df6bd9b24324cbeebcde9a2485
andrewDzokwom <andrewa441@gmail.com>
2025-01-21 13:30:18 +0100

    center the elements and adjust screen size for larger ones

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-/*************  ✨ Codeium Command ⭐  *************/
+const cors = require('cors');
 const express = require('express');
 const app = express();
+express.use(cors());
 app.use(express.static('pages'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/pages/index.html');
@@ -41,6 +42,26 @@ app.get('/south', (req, res) => {
 app.get('/far-north', (req, res) => {
     res.sendFile(__dirname + '/pages/far-north.html');
 });
+
+app.get("*", (req, res) => {
+    const answer = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>404</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>    
+        <h1>404</h1>
+        <article>
+            The page you are looking for does not exist.If you want to go back to the homepage please click the button below
+            <a href="/"><button>Back to Home</button></a>
+        </article>
+        `
+    res.send(answer)
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server listening on port 3000 at http://localhost:3000');
